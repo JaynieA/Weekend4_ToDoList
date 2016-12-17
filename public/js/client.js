cda8db14ccab321712475ca3aebc1af28e1b3de7
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-17 11:31:59 -0600

    basic post and get test routes working

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
-  console.log('JQ');
+  runGetRoute();
+  runPostRoute();
 });
+
+var runGetRoute = function() {
+  console.log('in runGetRoute');
+  $.ajax({
+    type: 'GET',
+    url: '/get',
+    success: function(response) {
+      console.log('get route success:', response);
+    }, // end success
+    error: function(err) {
+      console.log('get route error:', err);
+    } // end error
+  }); // end ajax
+}; // end runGetRoute
+
+var runPostRoute = function() {
+  console.log('in runPostRoute');
+  var objectToSend = {
+    thing: "soup"
+  }; // end objectToSend
+  $.ajax({
+    type: 'POST',
+    url: '/post',
+    data: objectToSend,
+    success: function(response) {
+      console.log(response);
+    }, // end success
+    error: function(err) {
+      console.log(err);
+    } // end error
+  }); // end ajax
+}; // end runPostRoute
--- server/app.js
+++ server/app.js
@@ -13,3 +13,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.listen(port, function() {
   console.log('server listening on', port);
 }); // end server listen
+
+//routes
+app.get('/get', function(req, res) {
+  console.log('get route hit');
+  res.send('GET OK');
+}); // end get route
+
+app.post('/post', function(req, res) {
+  console.log('post route hit');
+  res.send(req.body.thing);
+}); // end post route
